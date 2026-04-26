fded98918a58e1ea03f766f378c3a58ec58a069f
kindermaxii <noahhauwel1@gmail.com>
2026-04-27 01:43:55 +0200

    fqis: add global run audit report

--- app/fqis/reporting/__init__.py
+++ app/fqis/reporting/__init__.py
@@ -7,14 +7,28 @@ from app.fqis.reporting.hybrid_shadow_report import (
     load_hybrid_shadow_batch_records_from_jsonl,
     write_hybrid_shadow_batch_report_json,
 )
+from app.fqis.reporting.run_audit import (
+    RunAuditFlag,
+    RunAuditReport,
+    RunAuditThresholds,
+    build_run_audit_report,
+    run_audit_report_to_record,
+    write_run_audit_report_json,
+)
 
 __all__ = [
     "HybridShadowBatchReport",
     "NumericSummary",
+    "RunAuditFlag",
+    "RunAuditReport",
+    "RunAuditThresholds",
     "build_hybrid_shadow_batch_report_from_jsonl",
     "build_hybrid_shadow_batch_report_from_records",
+    "build_run_audit_report",
     "hybrid_shadow_batch_report_to_record",
     "load_hybrid_shadow_batch_records_from_jsonl",
+    "run_audit_report_to_record",
     "write_hybrid_shadow_batch_report_json",
+    "write_run_audit_report_json",
 ]
 
